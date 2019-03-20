d6e6851d591414fc4fc3d0b919f676e66f97bb5a
unknown <lappjeff20@gmail.com>
2019-03-20 18:49:19 -0400

    Cleaned up Todo component and TodoList component

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -3,11 +3,7 @@ import './Todo.css'
 
 function Todo (props) {
   return (
-    <div className='todo-container'>
-      <ul className='todos'>
         <li>{props.todos.task}</li>
-      </ul>
-    </div>
   )
 }
 export default Todo
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -5,12 +5,15 @@ import Todo from './Todo'
 // feel free to change this component.js into TodoList.js
 
 function TodoList (props) {
+  const todoItems = props.todos.map(todo =>
+    <Todo key={todo.id} todos={todo}/>
+  )
   return (
     <div className='list-container'>
       <h1>Todos</h1>
-      {props.todos.map(todo =>
-        <Todo key={todo.id} todos={todo}/>
-      )}
+      <ul className='todoItems'>
+        {todoItems}
+      </ul>
     </div>
   )
 }
