312568a85a94eca02f48aa58940b776820df9410
unknown <lappjeff20@gmail.com>
2019-03-21 17:56:52 -0400

    Set up SASS for styling

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Todo.css'
+import './Todo.scss'
 import Todo from './Todo'
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
